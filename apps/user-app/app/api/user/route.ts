a7a92c30bf2259a8b63c7c46640bdff4fe93a97f
Srijan <chaturchacharost@gmail.com>
2024-04-19 21:30:26 +0530

    checkpoint

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -1,22 +1,21 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../../lib/auth";
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@repo/db/client";
-
-const client = new PrismaClient();
 
 export const GET = async () => {
   try {
-    await client.user.create({
-      data: {
-        email: "asd",
-        name: "adsads",
-      },
-    });
+    const session = await getServerSession(authOptions);
+    if (session.user) {
+      return NextResponse.json({ user: session.user });
+    }
   } catch (error) {
-    return NextResponse.json({
-      message: error,
-    });
+    return NextResponse.json(
+      { message: "You are not logged in" },
+      { status: 403 }
+    );
   }
-  return NextResponse.json({
-    message: "hi there",
-  });
+  return NextResponse.json(
+    { message: "You are not logged in" },
+    { status: 403 }
+  );
 };
--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Page(): JSX.Element {
-  return <div className="text-5xl text-slate-900">hey from user app</div>;
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "../lib/auth";
+
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if (session?.user) {
+    redirect("/dashboard");
+  } else {
+    redirect("/api/auth/signin");
+  }
 }
