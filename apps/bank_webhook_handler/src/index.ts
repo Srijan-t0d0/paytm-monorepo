2b635cf12fa23f980ecf704550e0fea19d7d8dac
Srijan <chaturchacharost@gmail.com>
2024-04-20 21:46:29 +0530

    created a webhook server

---
+++ apps/bank_webhook_handler/src/index.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import db from "@repo/db/client";
+const app = express();
+
+app.post("/hdfcWebhook", async (req, res) => {
+  const paymentInfo: {
+    token: string;
+    userId: string;
+    amount: string;
+  } = {
+    token: req.body.token,
+    userId: req.body.userId,
+    amount: req.body.amount,
+  };
+
+  try {
+    await db.$transaction([
+      db.balance.updateMany({
+        where: { userId: Number(paymentInfo.userId) },
+        data: {
+          amount: {
+            increment: Number(paymentInfo.amount),
+          },
+        },
+      }),
+
+      db.onRampTransaction.updateMany({
+        where: {
+          token: paymentInfo.token,
+        },
+        data: {
+          status: "Success",
+        },
+      }),
+    ]);
+    res.json({
+      message: "Captured",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(411).json({ message: "Error while processing webhook" });
+  }
+});
+
+app.listen(3000);
