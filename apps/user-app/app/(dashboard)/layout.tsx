843bb9b34f6e2c1cc1fb65357767bbb4d22e92af
Srijan <chaturchacharost@gmail.com>
2024-04-20 19:14:34 +0530

    fixed jumping of side panel

--- apps/user-app/app/(dashboard)/layout.tsx
+++ apps/user-app/app/(dashboard)/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({
 }): JSX.Element {
   return (
     <div className="flex">
-      <div className="flex justify-center items-center w-72 border-r border-slate-300 min-h-screen mr-4">
+      <div className="flex  justify-center items-center max-w-72 min-w-72  border-r border-slate-300 min-h-screen mr-4">
         <div>
           <SidebarItem href={"/dashboard"} icon={<HomeIcon />} title="Home" />
           <SidebarItem
